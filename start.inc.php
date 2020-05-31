e42dac23e90022ae8414c2715d0f882e145b407a
dream-clou <64066471+dream-clou@users.noreply.github.com>
2020-05-31 17:42:00 +0800

    Create start.inc.php

---
+++ start.inc.php
@@ -0,0 +1,14 @@
+<html>
+<head>
+	<title>OOP用户模块</title>
+	<meta charset="utf-8">
+	<link rel="stylesheet" type="text/css" href="style.css">
+	</head>
+	<body>
+		<h3>欢迎登录OOP登录模式</h3>
+		<div class='start'>
+		<a href="index.php?index=login"><input class="btn" type="submit" name="send" value="登录"></a>
+		<a href="index.php?index=reg"><input class="btn" type="submit" name="send" value="注册"></a>
+</div>
+</body>
+</html>
